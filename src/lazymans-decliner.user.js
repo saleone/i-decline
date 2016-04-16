4323197480db50508614221105085fef9e38399d
Saa Saviæ <sasa.savic@protonmail.com>
2016-04-16 15:31:36 +0200

    Add support for remove Lounge trades

--- src/lazymans-decliner.user.js
+++ src/lazymans-decliner.user.js
@@ -6,16 +6,53 @@
 // @version 1.0.0
 // @include https://steamcommunity.com/id/*/tradeoffers/
 // @include http://steamcommunity.com/id/*/tradeoffers/
+// @include https://csgolounge.com/mytrades
+// @include http://csgolounge.com/mytrades
 // @grant none
 // ==/UserScript==
 
 console.log("Starting lazymans decliner.");
-divCloseAll = document.createElement("div");
-divCloseAll.className = "btn_darkblue_white_innerfade btn_medium new_trade_offer_btn responsive_OnClickDismissMenu";
-divCloseAll.innerHTML = "<span> Decline all trade offers...</span>";
-divCloseAll.style="margin-top: 10px";
-divCloseAll.on("click", declineEverything);
-document.querySelector(".rightcol_controls_content").appendChild(divCloseAll);
+
+// Extend array so it can search for values contained in itself
+Array.prototype.contains = function (value) {
+    for (var i in this) {
+        if (this[i] == value) {
+            return true;
+        }
+    }
+    return false;
+};
+
+// List of URLs that match the page we want
+urls_mytrades = ["https://csgolounge.com/mytrades",
+    "https://www.csgolounge.com/mytrades",
+    "http://csgolounge.com/mytrades",
+    "http://www.csgolounge.com/mytrades"];
+
+if (urls_mytrades.contains(window.location.href)) {
+    // We are on CS:GO Lounge trades page.
+    aDoneAll = document.createElement("a");
+    aDoneAll.id = "repost-button";
+    aDoneAll.className = "buttonright";
+    aDoneAll.innerHTML = "Mark all trades as done";
+    aDoneAll.addEventListener("click", function () {
+        var trades = document.querySelectorAll(".tradepoll");
+        for (var i = 0; i < trades.length; i++) {
+            trashTrade(trades[i].getAttribute("id").slice(5));
+        }
+    });
+aDoneAllParent = document.querySelector("article.standard");
+aDoneAllParent.insertBefore(aDoneAll, aDoneAllParent.firstChild);
+
+} else {
+    // We are not on CS:GO Lounge, so we must be on Steam.
+    divCloseAll = document.createElement("div");
+    divCloseAll.className = "btn_darkblue_white_innerfade btn_medium new_trade_offer_btn responsive_OnClickDismissMenu";
+    divCloseAll.innerHTML = "<span> Decline all trade offers...</span>";
+    divCloseAll.style="margin-top: 10px";
+    divCloseAll.addEventListener("click", declineEverything);
+    document.querySelector(".rightcol_controls_content").appendChild(divCloseAll);
+}
 
 function declineEverything() {
     var tradeOffers = document.querySelectorAll(".tradeoffer");
